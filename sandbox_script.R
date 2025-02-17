536cebfa6c05c8129789afacaf843803d8857549
Quinlan Mann <quinlanmmann@gmail.com>
2025-02-17 16:11:26 -0500

    Sandbox_code

--- sandbox_script.R
+++ sandbox_script.R
@@ -1,35 +1,16 @@
-library(tidyverse)
+MyFunc <- function(x) {
+  reshape2::dcast(data = x, formula = Dominant~Subordinate, value.var = "count")
+}
+list_of_objects <- lapply(X = list_of_objects, FUN = MyFunc)
 
-DS_HAY1_N %>% remove_rownames %>% column_to_rownames(var="Dominant")
-
-test_COl<-function(x){
+col2name<-function(x){
   x<-x%>%
-   remove_rownames%>%
+    remove_rownames%>%
     column_to_rownames(var = "Dominant")
 }
 
-list_of_objects<-lapply(list_of_objects, test_COl)
-NA_2_0<-function(x)
-{x[is.na(x)<-0]}
-
-lapply(list_of_objects, NA_2_0)
-list_of_objects[is.na(list_of_objects)]<-0
+list_of_objects<-lapply(list_of_objects, col2name)
 
 list_of_objects<-lapply(list_of_objects, function(x) replace(x, is.na(x), 0))
 
-
-## set up storage matrix
-# get names for row and columns
-matrix_maker<-function(x){
-  # construct 0 matrix of correct dimensions with row and column names
-  myMat <- matrix(0, 
-                  length(sort(unique(unlist(x[1:2])))), 
-                  length(sort(unique(unlist(x[1:2])))), 
-                  dimnames = list(sort(unique(unlist(x[1:2]))), 
-                                  sort(unique(unlist(x[1:2])))))
-  # fill in the matrix with matrix indexing on row and column names
-  myMat[as.matrix(x[c("Dominant", "Subordinate")])] <- x[["count"]]
-}
-matrixces<-lapply(list_of_objects, matrix_maker)
-
-matrixces$DS_EAST_CAMP_SE
+list2env(list_of_objects, .GlobalEnv)
