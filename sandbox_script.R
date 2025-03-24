faa26ba6e98643fde27c30d7121c1833a068c7de
Quinlan Mann <quinlanmmann@gmail.com>
2025-03-24 13:04:18 -0400

    CLEANED CODE AND ADDED RANDOMIZATION CODE IN CASE IT NEEDS TWEAKING IN MAIN

--- sandbox_script.R
+++ sandbox_script.R
@@ -1,4 +1,8 @@
-#Bootstrappign
+#this code is all supplementary versions
+#if anything is in here it may be used at a later date/time
+#all code will eventually be deleted or moved to the main script "domobs.Rmd"
+
+#Bootstrapping Stuff-----
 library(tidyverse)
 library(EloRating)
 
@@ -6,6 +10,8 @@ doms<-read.csv("C:\\Users\\quinl\\Desktop\\Book1.csv")
 
 set.seed(19970227) ## for reproducibility
 
+#this is all bootstrapping to see about including the weird copulations-----
+# this may be brought back for a future analysis but right now the decision is to keep it
 matrix_maker<-function(x){
   # construct 0 matrix of correct dimensions with row and column names
   myMat <- matrix(0, #filled with zeros
@@ -209,6 +215,7 @@ ggplot(df2, aes(normDS.y, normDS.x, label=ID))+
        y="Calculated David's scores\nfrom complete dataset")+
   ggthemes::theme_clean()
 
+#this was me playing with DS calculations and may come back to the normalization technique used-----
 DS(bonobos)
 bonobos<-bonobos
 bonobos2<-DS(bonobos)
@@ -227,7 +234,7 @@ DSPij<-function(x){
 DS(bonobos, prop = 'Pij')
 DSPij(bonobos)
 
-#If I want to make it so that they Norm Score is NOT corrected for chance
+#If I want to make it so that they Norm Score is NOT corrected for chance-----
 list_of_objects2<-lapply(list_of_objects, DSPij)
 David_Score2<-bind_rows(list_of_objects2, .id = "Group")
 David_Score2<-David_Score2 %>%
@@ -242,7 +249,7 @@ ggplot(David_Score2, aes(x=good_ranks, y=normDS))+
   facet_wrap(~Group)+
   ggthemes::theme_few()
 
-#Validating weord cody dey data
+#Validating weird cody dey data-----
 NPBNW<-readxl::read_xlsx("C:\\Users\\quinl\\Downloads\\Observation\\Observation\\NPB NW.xlsx")
 #DO NOT CHANGE
 NPBNW$Dominant[NPBNW$Dominant == 'WNBB'] <- 'YNBB'
@@ -268,19 +275,88 @@ NPBNW<-NPBNW[,-c(1)]%>%
 (DS(matrix_maker(NPBNW))
 ) %>% map_df(rev)
 
-ALLBANDS<-readxl::read_xlsx("C:\\Users\\quinl\\Downloads\\All Tawharanui Banded Pukeko_2022 (3).xlsx")
+#randomizing code-----
+reg_randomizer<-function(x){
+  #quick check to see if the matrix is square
+  x<-as.matrix(x)
+  if(length(rownames(x))!=length(colnames(x)))
+    return("the matrix is not square")
+  #adds the values of the matrix, and transposed matrix together to get
+  #the sum of interactions
+  x<-x+t(x)
+  #removes the lower half so I am not adding the sum twice
+  x[lower.tri(x)] <- 0  
+  x<-as.data.frame(x)
+  #creates the winner ID column so I can make the dataframe long
+  x<-tibble::rownames_to_column(x, "BIRD1")
+  #unique total value of columns per matrix
+  N<-length(colnames(x))
+  #creates a unique dataframe that has a list of all interaction pairs (totals)
+  x<-pivot_longer(as.data.frame(x), 
+                  cols = c(2:N), 
+                  names_to = "BIRD2", 
+                  values_to = "INT")
+  x<-filter(x, INT > 0)
+  #creating a random number of wins/losses conditional on how many interactions
+  x$WIN<-sapply(x$INT, function(y) sample(0:y, 1))
+  x$LOSS<-x$INT-x$WIN
+  #creating two dataframes of wins and losses
+  wins<-x[,c(1,2,4)]
+  wins<-wins%>%
+    rename(Dominant=BIRD1, 
+           Subordinate=BIRD2, 
+           count=WIN)
+  loss<-x[,c(2,1,5)]
+  loss<-loss%>%
+    rename(Dominant=BIRD2, 
+           Subordinate=BIRD1, 
+           count=LOSS)
+  #binding the new dataframes
+  x<-rbind(wins,loss)
+  #making them back to a matrix
+  myMat <- matrix(0, #filled with zeros
+                  length(sort(unique(unlist(x[1:2])))), #length of all unique bird name values in rows and columns
+                  length(sort(unique(unlist(x[1:2])))), #length of all unique bird name values in rows and columns
+                  dimnames = list(sort(unique(unlist(x[1:2]))), #naming rows and columns after all unique bird name values in dataframe
+                                  sort(unique(unlist(x[1:2])))))
+  # fill in the matrix with matrix indexing on row and column names
+  myMat[as.matrix(x[c("Dominant", "Subordinate")])] <- x[["count"]]
+  #DS calculation from EloRating Package
+  rn <- rownames(myMat)
+  cn <- colnames(myMat)
+  tmat <- t(myMat)
+  summatrix <- myMat + tmat
+  diag(summatrix) <- 0
+  summatrix <- replace(summatrix, summatrix == 0, NA)
+  l1 <- which(is.na(summatrix), arr.ind = TRUE)
+  #this is using the Dij calculation change if you decide to use the Pij calculation
+  propmatrix <- (myMat + 0.5)/(tmat + myMat + 1)
+  propmatrix <- replace(propmatrix, l1, 0)
+  w <- rowSums(propmatrix)
+  w2 <- propmatrix %*% w
+  l <- rowSums(t(propmatrix))
+  l2 <- t(propmatrix) %*% l
+  DS <- w + w2 - l - l2
+  normDS <- ((DS + ((length(DS)) * (length(DS) - 1))/2))/length(DS)
+  good_ranks <- order(order(normDS, decreasing=TRUE))
+  res <- data.frame(ID = rn, DS = DS, normDS = normDS, good_ranks = good_ranks)
+  res <- res[order(res$DS, decreasing = TRUE), ]
+  rownames(res) <- NULL
+  #return(res)
+  r2<-summary(lm(normDS ~ good_ranks, data = res))[["coefficients"]][2]
+  return(r2)
+}
 
-xxx<-merge(David_Score, ALLBANDS, by.x = "ID", by.y = "Colour.bands")[,c(1:7)]
-xxx<-unique(xxx)
+results <- data.frame(matrix(ncol = length(list_of_objects2), nrow = 1000))
+x <- unique(ls(list_of_objects2))
+colnames(results) <- x
 
-PPO<-readxl::read_xlsx("C:\\Users\\quinl\\Downloads\\PPO_MASTERLIST_2024 (2).xlsx")
-PPO<-PPO[,c(1,7,9)]
+for(i in 1:length(list_of_objects2))
+{
+  values <- list_of_objects2[[i]]
+  results[, i] <- as.numeric(replicate(1000, 
+                                       reg_randomizer(values), 
+                                       simplify = FALSE))
+  
+}
 
-xxx2<-merge(xxx, PPO, by.x = "number", by.y="Below Knee Left")
-
-write.csv(xxx2, "C:\\Users\\quinl\\Downloads\\xxx2.csv")
-
-xxx2$Band.Number<-gsub("M-", "", as.character(xxx2$Band.Number))
-
-xxx2$TF<-if_else(xxx2$Band.Number==xxx2$number, "T", "F")
-write.csv(David_Score, "C:\\Users\\quinl\\Downloads\\xxx3.csv")
